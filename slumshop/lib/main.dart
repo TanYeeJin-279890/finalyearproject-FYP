cdba9589adcd5c9bde1ef3413d4a2dfcfbc192b6
TanYeeJin-279890 <tan_yee_jin@soc.uum.edu.my>
2022-05-06 14:25:47 +0800

    new

--- slumshop/lib/main.dart
+++ slumshop/lib/main.dart
@@ -1,6 +1,6 @@
-//import 'dart:async';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-//import 'package:slumshop/views/mainscrn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SlumShop',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: const MySplashScreen(title: 'SlumShop Admin'),
+      title: 'Slumshop Admin',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MySplashScreen(title: 'Slumshop Admin'),
     );
   }
 }
@@ -22,23 +25,59 @@ class MyApp extends StatelessWidget {
 class MySplashScreen extends StatefulWidget {
   const MySplashScreen({Key? key, required String title}) : super(key: key);
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   @override
   State<MySplashScreen> createState() => _MySplashScreenState();
 }
 
 class _MySplashScreenState extends State<MySplashScreen> {
+  late double screenHeight, screenWidth;
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (content) => const MainScreen())));
+  }
+
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/1.png'), fit: BoxFit.cover),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(64.0),
+              child: Image.asset('assets/images/1.png'),
             ),
-          ),
-        ],
+            const Text(
+              "SlumShop (Admin)",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const CircularProgressIndicator(),
+            const Text("Version 0.1",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                )),
+          ],
+        ),
       ),
     );
   }
