fb3d06f2c114262c592997fca67259f33d140fff
TanYeeJin-279890 <tan_yee_jin@soc.uum.edu.my>
2022-05-18 22:02:47 +0800

    new

---
+++ my_tutor/lib/views/user_registration.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: const Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- my_tutor/lib/views/userlogin.dart
+++ my_tutor/lib/views/userlogin.dart
@@ -1,19 +1,140 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+import 'user_registration.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late double screenHeight, screenWidth, ctrwidth;
+  bool remember = false;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth >= 800) {
+      ctrwidth = screenWidth / 1.5;
+    }
+    if (screenWidth < 800) {
+      ctrwidth = screenWidth;
+    }
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-      ),
-      body: Center(
-        child: Container(
-          child: const Text('Hello World'),
+        body: SingleChildScrollView(
+      child: Center(
+        child: SizedBox(
+          width: ctrwidth,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(32, 32, 32, 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                          height: screenHeight / 2.5,
+                          width: screenWidth,
+                          child: Image.asset('assets/images/tutor.png')),
+                      const Text(
+                        "Login",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0))),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter valid email';
+                          }
+                          bool emailValid = RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value);
+
+                          if (!emailValid) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0))),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          if (value.length < 6) {
+                            return "Password must be at least 6 characters";
+                          }
+                          return null;
+                        },
+                      ),
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: remember,
+                            onChanged: (bool? value) {
+                              _onRememberMeChanged(value!);
+                            },
+                          ),
+                          const Text("Remember Me")
+                        ],
+                      ),
+                      SizedBox(
+                        width: screenWidth,
+                        height: 50,
+                        child: ElevatedButton(
+                          child: const Text("Login"),
+                          onPressed: _loginUser,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        child: const Text("Not Yet Registered? Click Me!",
+                            style: TextStyle(color: Colors.blue)),
+                        onTap: () {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const RegisterPage()));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-    );
+    ));
   }
+
+  void _loginUser() {}
+
+  void _onRememberMeChanged(bool bool) {}
 }
