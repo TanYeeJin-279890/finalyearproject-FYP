81ed8382e4126e0148b9644cdfdbab6e8e8d9d7a
TanYeeJin-279890 <tan_yee_jin@soc.uum.edu.my>
2023-05-13 17:39:41 +0800

    latest

--- androidx/android/app/src/main/kotlin/com/example/androidx/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidx
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- androidx/lib/views/constant.dart
+++
@@ -1,8 +0,0 @@
-class CONSTANTS {
-  //static String server = "http://10.19.4.86"; //office
-  //static String server = "http://192.168.56.1";
-  //static const String server = "http://192.168.0.155";
-  //static String server = "http://10.19.28.22"; //uum wireless lan
-  //static String server = "http://10.19.28.22"; //mcmc
-  static String server = "http://10.19.43.207"; //hijau kuning
-}
